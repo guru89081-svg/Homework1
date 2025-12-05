77db9d5f36e5a2a9d5c4d7ee13bc0c6d6030e138
Guruprasad <guru89081@gmail.com>
2025-12-05 20:20:53 +0530

    Add files via upload

---
+++ patternProgram.java
@@ -0,0 +1,19 @@
+package guru.com;
+
+public class patternProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+		        for (int i = 0; i < 5; i++) {
+		         for (int j = 5; j > i; j--) {
+		                System.out.print("*");
+		            }
+		            System.out.println();
+		        }
+		    }
+		}
+	
+
+
